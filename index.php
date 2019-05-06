c8a7369689fa1127dfa235c564ba7555e65a215e
Laurine Merault <laurine.merault@viacesi.fr>
2019-05-06 16:08:19 +0200

    inclusions fichiers

--- index.php
+++ index.php
@@ -1,27 +1,6 @@
 <?php
 
-/*
-$version ="7";
-$message = '<h1>Je fais du PHP' . $version . '</h1>';
-echo $message;
-
-//Utilisation des constantes
-define('TVAxPlein', 0.2);
-echo TVAxPlein;
-
-*/
-
-$resultat = '<ul>';
-for ($i = -12328; $i <= 23456 ; $i = $i += 7) {
-    if ( $i % 5 == 0){
-        $resultat .= '<li>' . $i . '</li>' ;
-    }
-    
-
-}
-
-$resultat .= '</ul>';
-
-echo $resultat;
-
+require_once './includes/header.php';
+require_once './includes/content.php';
+require_once './includes/footer.php';
 
