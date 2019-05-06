9a737e5b5383a754d115e9de7a8e541edf60aa07
Laurine Merault <laurine.merault@viacesi.fr>
2019-05-06 14:58:05 +0200

    Boucle for

--- index.php
+++ index.php
@@ -12,12 +12,16 @@ echo TVAxPlein;
 */
 
 $resultat = '<ul>';
-for ($i = 1 ; $i <= 100 ; $i ++) {
-    $resultat .= '<li>' . $i . '</li>' ;
+for ($i = -12328; $i <= 23456 ; $i = $i += 7) {
+    if ( $i % 5 == 0){
+        $resultat .= '<li>' . $i . '</li>' ;
+    }
+    
 
 }
 
 $resultat .= '</ul>';
 
 echo $resultat;
+
 
