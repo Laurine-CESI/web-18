637639bc8b56d10340f9293d5e5bdc53cdcac50c
Laurine Merault <laurine.merault@viacesi.fr>
2019-05-06 14:32:54 +0200

    Boucles

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+/*
 $version ="7";
 $message = '<h1>Je fais du PHP' . $version . '</h1>';
 echo $message;
@@ -7,3 +8,16 @@ echo $message;
 //Utilisation des constantes
 define('TVAxPlein', 0.2);
 echo TVAxPlein;
+
+*/
+
+$resultat = '<ul>';
+for ($i = 1 ; $i <= 100 ; $i ++) {
+    $resultat .= '<li>' . $i . '</li>' ;
+
+}
+
+$resultat .= '</ul>';
+
+echo $resultat;
+
