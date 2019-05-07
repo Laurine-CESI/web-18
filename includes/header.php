1745c08502177a770d0ba2a8eb76a21cf25af83e
Laurine Merault <laurine.merault@viacesi.fr>
2019-05-07 09:45:51 +0200

    Ajout lien vers page d'inscription

--- includes/header.php
+++ includes/header.php
@@ -3,6 +3,7 @@
         <ul>
             <li><a href="index.php?page=home" title="Page d'accueil"> Accueil </a></li>
             <li><a href="index.php?page=article" title="news" hreflang="en"> Actualités</a></li>
+            <li><a href="index.php?page=registration" title="Inscription" hreflang="en"> Inscription</a></li>
         </ul>
     </nav>
 </header>
