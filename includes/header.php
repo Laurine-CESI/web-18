0a212d15d41473642c3d5649ccdfb24f925910bc
Laurine Merault <laurine.merault@viacesi.fr>
2019-05-07 09:43:36 +0200

    Récupération fonction getLang

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,7 @@
     <nav>
         <ul>
             <li><a href="index.php?page=home" title="Page d'accueil"> Accueil </a></li>
-            <li><a href="index.php?page=article" title="news"> Actualités</a></li>
+            <li><a href="index.php?page=article" title="news" hreflang="en"> Actualités</a></li>
         </ul>
     </nav>
 </header>
